42e7ffc872aa49a1fafc96b6504c61098da7eef8
Megan McCreight <appacademy.megan@gmail.com>
2014-11-13 14:54:24 -0800

    Init Commit

---
+++ android/src/com/mccreightm/platformer/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mccreightm.platformer.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mccreightm.platformer.Platformer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Platformer(), config);
+	}
+}
