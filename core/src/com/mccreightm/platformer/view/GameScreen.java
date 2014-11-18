75f67796a839e1cc62edb8da31903b41069aafd1
Megan McCreight <appacademy.megan@gmail.com>
2014-11-18 14:56:18 -0800

    Tuesday, November, 18, 2014 Loading Tiled map in the game. Launching game in both Desktop and Android configurations. Push project to GitHub.

---
+++ core/src/com/mccreightm/platformer/view/GameScreen.java
@@ -0,0 +1,56 @@
+package com.mccreightm.platformer.view;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class GameScreen implements Screen{
+    public TiledMap map;
+    public OrthogonalTiledMapRenderer renderer;
+    public OrthographicCamera camera;
+//initializes other thingies
+    public GameScreen() {
+        map = new TmxMapLoader().load("map/level01.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1/70f);
+        camera = new OrthographicCamera(14f, 14f);
+    }
+//renders the thingy
+    @Override
+    public void render(float delta) {
+        camera.update();
+        renderer.setView(camera);
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
