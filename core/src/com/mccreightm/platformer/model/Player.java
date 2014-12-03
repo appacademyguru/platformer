702c3c4947451d249ed432bcc6869cb895c128f6
Megan McCreight <appacademy.megan@gmail.com>
2014-12-03 15:04:38 -0800

    12/3/14 - drew sprite sheets

--- core/src/com/mccreightm/platformer/model/Player.java
+++ core/src/com/mccreightm/platformer/model/Player.java
@@ -2,6 +2,7 @@ package com.mccreightm.platformer.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
@@ -12,5 +13,14 @@ public class Player {
         //set sprite init position and connect spritesheet
         position = new Vector2(0,0);
         spriteSheet = new Texture(Gdx.files.internal("img/aliens.png"));
+    }
+    //draw character on screen
+    public void draw(Batch spriteBatch){
+        //put whole sprite sheet on screen
+        spriteBatch.draw(spriteSheet, 0, 0, 70, 100);
+    }
+    //update characteristics of character
+    public void update(float deltaTime){
+
     }
 }
--- core/src/com/mccreightm/platformer/view/GameScreen.java
+++ core/src/com/mccreightm/platformer/view/GameScreen.java
@@ -4,14 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.mccreightm.platformer.model.Player;
 
 public class GameScreen implements Screen{
     public TiledMap map;
     public OrthogonalTiledMapRenderer renderer;
     public OrthographicCamera camera;
+
+    public SpriteBatch spriteBatch;
+    public Player player;
+
 //constructor
     public GameScreen() {
         //loads map
@@ -25,17 +31,31 @@ public class GameScreen implements Screen{
         camera = new OrthographicCamera(14f, 14f * (height / width));
         //sets the camera position to the view width and height divided by two so that the view is aligned to the bottom
         camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0f);
+
+        //create new spritebatch object: groups sprite sheets and textures so they can be drawn efficiently
+        spriteBatch = new SpriteBatch();
+        //create new player object
+        player = new Player();
     }
-//renders the thingy
+//draws stuff on the screen
     @Override
     public void render(float delta) {
+      //background color
         //sets color of the clear
         Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+      //background tile layer
         //sets the camera view and updates it constantly
         camera.update();
         renderer.setView(camera);
         renderer.render();
+      //character layer
+        //get ready to begin drawing
+        spriteBatch.begin();
+        //draw the player on the screen using the spriteBatch object
+        player.draw(spriteBatch);
+        //stop drawing
+        spriteBatch.end();
     }
 
     @Override
