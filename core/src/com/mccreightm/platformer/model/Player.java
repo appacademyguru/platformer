c1f75c2e06f8d763aecfca039c5615ede9c5420e
Megan McCreight <appacademy.megan@gmail.com>
2014-12-09 14:54:11 -0800

    12/9/14 - animated the sprite

--- core/src/com/mccreightm/platformer/model/Player.java
+++ core/src/com/mccreightm/platformer/model/Player.java
@@ -2,6 +2,7 @@ package com.mccreightm.platformer.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -12,6 +13,9 @@ public class Player {
     public Vector2 position;
     public Texture spriteSheet;
     public TextureRegion[] spriteFrames;
+
+    public Animation animation; //stores one animation
+    private float stateTime;
 
     public Player() {
         //set sprite init position and connect spritesheet
@@ -37,14 +41,21 @@ public class Player {
 
         }
 
+        TextureRegion[] animationFrames = new TextureRegion[2];//create new texture region array to store animation frames
+        animationFrames[0] = spriteFrames[45]; //stores sprite 51 in animationFrames
+        animationFrames[1] = spriteFrames[46]; //stores sprite 52 in animationFrames
+        animation = new Animation(0.3f, animationFrames);//create new animation
+        stateTime = 0f; //initialize stateTime
+
     }
     //draw character on screen
     public void draw(Batch spriteBatch){
         //draw sprite on screen
-        spriteBatch.draw(spriteFrames[44], position.x, position.y, 70 * (1/70f), 100 * (1/70f));
+        spriteBatch.draw(animation.getKeyFrame(stateTime, true), position.x, position.y, 70 * (1/70f), 100 * (1/70f));//animate sprite
     }
     //update characteristics of character
     public void update(float deltaTime){
-        position.x += deltaTime;
+        stateTime += deltaTime;
+        position.y += deltaTime;//direction of sprite movement
     }
 }
