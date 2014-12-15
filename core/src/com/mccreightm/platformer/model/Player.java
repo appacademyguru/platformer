3e093093212ca25fc8d2033330ce2bfae003957c
Megan McCreight <appacademy.megan@gmail.com>
2014-12-15 15:05:40 -0800

    12/15/14 - made hash map for animations and stored all of the animations

--- core/src/com/mccreightm/platformer/model/Player.java
+++ core/src/com/mccreightm/platformer/model/Player.java
@@ -8,38 +8,44 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import java.sql.Time;
+import java.util.HashMap;
 
 public class Player {
     public Vector2 position;
-    public Animation animation;
-    public Animation animation2;
     public Spritesheet spriteSheet;
 
     public int width;
     public int height;
 
     private float stateTime;
+    private HashMap<String, Animation> animations;
 
     public Player() {
         //set sprite init position and connect spritesheet
         position = new Vector2(14,4);
+        animations = new HashMap<String, Animation>();
         width = 70;
         height = 100;
         spriteSheet = new Spritesheet("img/aliens.png", width, height);//create spriteSheet object
-        animation = spriteSheet.createAnimation(53, 54, 0.3f);
-        animation2 = spriteSheet.flipAnimation(animation, true, false);
+        animations.put("walkRight", spriteSheet.createAnimation(53, 54, 0.3f));
+        animations.put("walkLeft", spriteSheet.flipAnimation(animations.get("walkRight"), true, false));
+        animations.put("swimRight", spriteSheet.createAnimation(51, 52, 0.3f));
+        animations.put("swimLeft", spriteSheet.flipAnimation(animations.get("swimRight"), true, false));
+        animations.put("jumpRight", spriteSheet.createAnimation(48, 49, 0.3f));
+        animations.put("jumpLeft", spriteSheet.flipAnimation(animations.get("jumpRight"), true, false));
+        animations.put("stand", spriteSheet.createAnimation(44, 44, 0.3f));
+        animations.put("climb", spriteSheet.createAnimation(45, 46, 0.3f));
+        animations.put("duck", spriteSheet.createAnimation(47, 47, 0.3f));
         stateTime = 0f; //initialize stateTime
-
-
     }
     //draw character on screen
     public void draw(Batch spriteBatch){
         //draw sprite on screen
-        spriteBatch.draw(animation.getKeyFrame(stateTime, true), position.x, position.y, 70 * (1/70f), 100 * (1/70f));//animate sprite
+        spriteBatch.draw(animations.get("duck").getKeyFrame(stateTime, true), position.x, position.y, 70 * (1/70f), 100 * (1/70f));//animate sprite
     }
     //update characteristics of character
     public void update(float deltaTime){
         stateTime += deltaTime;
-        position.x -= deltaTime;//direction of sprite movement
+        position.x += deltaTime;//direction of sprite movement
     }
 }
